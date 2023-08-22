ce5318c5792132ae1c727cb0fda904bd4329aaaa
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-22 19:54:12 +0800

    feat: can get company by id

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,9 @@ public class CompanyController {
     public List<Company> getCompanyList() {
 
         return companyRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Company findCompanyById(@PathVariable Long id) {
+        return companyRepository.findById(id);
     }
 }
--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.springbootemployee.repository;
 
+import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,14 +11,23 @@ import java.util.List;
 @Repository
 public class CompanyRepository {
     private static final List<Company> companyList = new ArrayList<>();
-    static{
-        companyList.add(new Company(1,"OOCL"));
-        companyList.add(new Company(2,"KSK company"));
-        companyList.add(new Company(3,"JLJ company"));
+
+    static {
+        companyList.add(new Company(1, "OOCL"));
+        companyList.add(new Company(2, "KSK company"));
+        companyList.add(new Company(3, "JLJ company"));
     }
 
 
     public List<Company> findAll() {
         return companyList;
+    }
+
+    public Company findById(Long id) {
+
+        return companyList.stream()
+                .filter(company -> company.getId() == id)
+                .findFirst()
+                .orElseThrow(EmployeeNotFoundException::new);
     }
 }
