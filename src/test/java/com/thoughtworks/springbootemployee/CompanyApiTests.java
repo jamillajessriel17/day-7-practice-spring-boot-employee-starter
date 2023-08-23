898e6ffcb7398e45afde8bc23f5380f429274e83
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-23 21:11:23 +0800

    test: create integration test for get company by page number and page size

--- src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
@@ -10,6 +10,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -56,7 +60,7 @@ public class CompanyApiTests {
     }
 
     @Test
-    void should_return_employee_list_when_get_employees_by_company_id() throws Exception {
+    void should_return_employee_list_when_perform_get_employees_by_company_id() throws Exception {
         //given
         Long companyId = 1L;
         Employee jessriel = new Employee(null, "Jessriel", 23, "male", 345, 1L);
@@ -67,7 +71,7 @@ public class CompanyApiTests {
         Employee jessrielEmployee = companyRepository.getEmployeeRepository().saveEmployee(jessriel);
         companyRepository.getEmployeeRepository().saveEmployee(lucy);
         Employee danEmployee = companyRepository.getEmployeeRepository().saveEmployee(dan);
-        //when
+        //when  //then
         mockMvcClient.perform(MockMvcRequestBuilders.get("/companies/" + companyId + "/employees"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
@@ -75,6 +79,29 @@ public class CompanyApiTests {
                 .andExpect(jsonPath("$[0].name").value(jessrielEmployee.getName()))
                 .andExpect(jsonPath("$[1].id").value(danEmployee.getId()))
                 .andExpect(jsonPath("$[1].name").value(danEmployee.getName()));
+
+    }
+
+    @Test
+    void should_return_company_list_when_perform_get_companies_given_page_number_page_size() throws Exception {
+        //given
+        companyRepository.saveCompany(new Company("Hello"));
+        companyRepository.saveCompany(new Company("Hello1"));
+        companyRepository.saveCompany(new Company("Hello2"));
+        Company hello3Company = companyRepository.saveCompany(new Company("Hello3"));
+        Company hello4Company = companyRepository.saveCompany(new Company("Hello4"));
+        //when
+        mockMvcClient.perform(MockMvcRequestBuilders.get("/companies")
+                        .param("pageNumber", "2")
+                        .param("pageSize", "3"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(hello3Company.getId()))
+                .andExpect(jsonPath("$[0].name").value(hello3Company.getName()))
+                .andExpect(jsonPath("$[1].id").value(hello4Company.getId()))
+                .andExpect(jsonPath("$[1].name").value(hello4Company.getName()));
+        ;
+        ;
         //then
     }
 }
