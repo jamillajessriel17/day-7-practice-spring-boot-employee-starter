af0f6c79d9238605a693b748f90b5d6ac5b891a7
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-24 09:14:38 +0800

    feat: return bad request for inactive exception

--- src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeInactiveException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeInactiveException.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.springbootemployee.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeInactiveException extends RuntimeException {
     public EmployeeInactiveException() {
         super("Employee is inactive.");
